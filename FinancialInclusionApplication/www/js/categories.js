6eb3eeb08c6f60297dd36d07f009f90d61d6f760
cameroncclark <xeb12181@uni.strath.ac.uk>
2016-12-13 14:03:29 +0000

    Updated categories

--- FinancialInclusionApplication/www/js/categories.js
+++ FinancialInclusionApplication/www/js/categories.js
@@ -1,6 +1,6 @@
 fIApp.controller('CategoriesCtrl', function($scope){
-    var categories = [{name:'Banking', url:'banking'}, {name:'Tax', url:'tax'},
-        {name:'Savings', url:'savings'}];
+    var categories = [{name:'Banking', url:'banking', progress:80}, {name:'Tax', url:'tax', progress:50},
+        {name:'Savings', url:'savings', progress:10}];
     var subCategories = [{name:'Current Accounts',url:'current_account'}]
     $scope.getCategoryValues = function(){
         return categories;
