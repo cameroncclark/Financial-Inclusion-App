2416e234a86f1682d8452e8f8871a2f6ef6f3834
Angus Arnold <angusarnold@Anguss-MacBook-Pro.local>
2017-01-04 14:39:15 +0000

    Completed useful numbers and external links

--- FinancialInclusionApplication/www/js/controllers/InformationPage/externalLinksControllers.js
+++ FinancialInclusionApplication/www/js/controllers/InformationPage/externalLinksControllers.js
@@ -1,4 +1,13 @@
 fIApp.controller('ExternalLinksCtrl', function($scope){
     $scope.name = 'External Links';
     $scope.content = 'External Links stored here';
+
+    $scope.externalLinksList = [
+        {name: 'Facebook', website: 'http://www.facebook.com'},
+        {name: 'YouTube', website: 'http://www.youtube.com'},
+        {name: 'Twitter', website: 'http://www.twitter.com'}];
+
+    $scope.getExternalLinks = function(){
+        return $scope.externalLinksList;
+    };
 });
--- FinancialInclusionApplication/www/js/controllers/InformationPage/usefulNumbersControllers.js
+++ FinancialInclusionApplication/www/js/controllers/InformationPage/usefulNumbersControllers.js
@@ -1,4 +1,14 @@
 fIApp.controller('UsefulNumbersCtrl', function($scope){
     $scope.name = 'Useful Numbers';
     $scope.content = 'Useful Numbers will be stored here';
+    
+    // Information on useful numbers. website may be redundant for this purpose
+    $scope.numbersList = [
+            {name:'A Useful Company', number:'01411234567'},
+            {name:'Another Useful Company', number:'01411234568'},
+            {name:'Stuart Cuthbertson', number:'07766624002'}];
+
+    $scope.getInfoCategoryValues = function(){
+        return $scope.numbersList;
+    };
 });
