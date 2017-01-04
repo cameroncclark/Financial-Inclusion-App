0239d784e8b6f20f6d6d0f3b8bea66d0ad06861c
Stuart <StuCuth@Outlook.com>
2017-01-04 12:16:20 +0000

    Worked on categories page, now ready to accept and link into content

--- FinancialInclusionApplication/www/js/controllers/categoryControllers.js
+++ FinancialInclusionApplication/www/js/controllers/categoryControllers.js
@@ -1,3 +1,36 @@
-fIApp.controller('CategoryCtrl', function($scope){
-    $scope.name = 'Categories Page';
+fIApp.controller('CategoryCtrl', function ($scope, $ionicModal) {
+  $scope.name = 'Categories Page';
+  $scope.modalSubCategories = [];
+
+  $scope.categories = [{
+    "categoryName": "Banking",
+    "subCategories": ["Saving money", "APR", "Interest rates"],
+    "progress": 50
+  }, {
+    "categoryName": "Finance",
+    "subCategories": ["Managing money", "Credit ratings"],
+    "progress": 70
+  }];
+
+  $ionicModal.fromTemplateUrl('templates/subCategoriesModal.html', {
+    scope: $scope
+  }).then(function (modal) {
+    $scope.subCategoriesModal = modal;
+  });
+
+  $scope.launchCategory = function (category) {
+      $scope.modalSubCategories = category.subCategories;
+      $scope.subCategoriesModal.show();
+  }
+
+  $scope.closeModal = function(){
+      $scope.modalSubCategories = [];
+      $scope.subCategoriesModal.hide();
+  }
+
+  $scope.log = function (name) {
+    console.log(name);
+  }
+
+
 });
