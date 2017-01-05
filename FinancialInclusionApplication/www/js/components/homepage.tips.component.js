b6debb2249b5a9c15787c1f0166ce40f555ba638
cameroncclark <xeb12181@uni.strath.ac.uk>
2017-01-05 15:02:14 +0000

    Made tips have it's own JSON file.
    Tips are now found in their own external JSON file instead of hard coded
    in the component.

--- FinancialInclusionApplication/www/js/components/homepage.tips.component.js
+++ FinancialInclusionApplication/www/js/components/homepage.tips.component.js
@@ -7,50 +7,13 @@ fIApp.service('randomHint', function(){
 
 fIApp.component("tipComponent",{
     templateUrl:"templates/tips.html",
-    controller: function testController($scope, randomHint){
+    controller: function testController($scope, randomHint, $http){
 
-        $scope.tips = [
-            {
-                "header":"Tip header 1",
-                "tip":"This is tip 1."
-            },
-            {
-                "header":"Tip header 2",
-                "tip":"This is tip 2."
-            },
-            {
-                "header":"Tip header 3",
-                "tip":"This is tip 3."
-            },
-            {
-                "header":"Tip header 4",
-                "tip":"This is tip 4."
-            },
-            {
-                "header":"Tip header 5",
-                "tip":"This is tip 5."
-            },
-            {
-                "header":"Tip header 6",
-                "tip":"This is tip 6."
-            },
-            {
-                "header":"Tip header 7",
-                "tip":"This is tip 7."
-            },
-            {
-                "header":"Tip header 8",
-                "tip":"This is tip 8."
-            },
-            {
-                "header":"Tip header 9",
-                "tip":"This is tip 9."
-            },
-            {
-                "header":"Tip header 10",
-                "tip":"This is tip 10."
-            },
-        ];
+        //Get the tips from an external JSON file
+        $http.get('content/tips.json')
+        .then(function (tips) {
+            $scope.tips = tips.data;
+        });
 
         $scope.index = randomHint.generate();
 
