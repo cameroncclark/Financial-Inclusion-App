20d94a7aff5004d9f9cd9ec877f49bf125975313
cameroncclark <xeb12181@uni.strath.ac.uk>
2017-01-09 11:28:02 +0000

    Bug Fixes

--- FinancialInclusionApplication/www/js/controllers/InformationPage/usefulNumbersControllers.js
+++ FinancialInclusionApplication/www/js/controllers/InformationPage/usefulNumbersControllers.js
@@ -3,11 +3,11 @@ fIApp.controller('UsefulNumbersCtrl', function($scope){
     $scope.content = 'Useful Numbers will be stored here';
     
     $scope.numbersList = [
-            {name:'A Useful Company', number:'01411234567'},
-            {name:'Another Useful Company', number:'01411234568'},
-            {name:'Stuart Cuthbertson', number:'07766624002'},
-            {name:'Angus Arnold', number:'07545616173'},
-            {name:'Cameron Clark', number:'07788464196'}];
+            {name:'A Useful Company', number:'+441411234567'},
+            {name:'Another Useful Company', number:'+441411234568'},
+            {name:'Stuart Cuthbertson', number:'+447766624002'},
+            {name:'Angus Arnold', number:'+447545616173'},
+            {name:'Cameron Clark', number:'+447788464196'}];
 
     $scope.getInfoCategoryValues = function(){
         return $scope.numbersList;
