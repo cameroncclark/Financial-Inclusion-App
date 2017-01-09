539d728bc04fb4b709fce2bdd28eaa2f7b05096f
Stuart <StuCuth@Outlook.com>
2017-01-09 11:58:20 +0000

    Content is now swipable through sections, need to parse individual pages now to style based on our included tags

--- FinancialInclusionApplication/www/js/controllers/contentController.js
+++ FinancialInclusionApplication/www/js/controllers/contentController.js
@@ -1,6 +1,7 @@
 fIApp.controller('ContentCtrl', function ($scope, $http, $stateParams, $sce) {
   $scope.name = $stateParams.pageTitle;
   $scope.content = "";
+  $scope.sections = [];
   var ourTags = ["<section>", "</section>", "<title>", "</title>"];
 
   $http.get('content/topics/' + $stateParams.contentURL)
@@ -10,7 +11,7 @@ fIApp.controller('ContentCtrl', function ($scope, $http, $stateParams, $sce) {
     });
 
   $scope.parse = function (content) {
-    var sections = [];
+    $scope.sections = [];
     var openCounter = null;
     var closeCounter = null;
 
@@ -20,30 +21,52 @@ fIApp.controller('ContentCtrl', function ($scope, $http, $stateParams, $sce) {
     } catch (error) {
       content = "Invalid file representation."
     }
-    
 
-    if(openCounter == null){
+
+    if (openCounter == null) {
       openCounter = 0;
     }
 
-    if(closeCounter == null){
+    if (closeCounter == null) {
       closeCounter = 0;
     }
 
-    if (openCounter === closeCounter && openCounter > 0 && closeCounter > 0 ) {
-      for(var i = 0 ; i < openCounter; i++){
-        var sectionStartIndex = content.indexOf('<section>')+9;
-        var sectionEndIndex = content.indexOf('</section>',sectionStartIndex);
-        var sectionContent = content.substring(sectionStartIndex,sectionEndIndex);
-        sections.push(sectionContent);
-        content = content.substring(sectionEndIndex+10);
+    if (openCounter === closeCounter && openCounter > 0 && closeCounter > 0) {
+      for (var i = 0; i < openCounter; i++) {
+        var sectionStartIndex = content.indexOf('<section>') + 9;
+        var sectionEndIndex = content.indexOf('</section>', sectionStartIndex);
+        var sectionContent = content.substring(sectionStartIndex, sectionEndIndex);
+        $scope.sections.push(sectionContent);
+        content = content.substring(sectionEndIndex + 10);
       }
-      console.log(sections);
-      
+
     } else {
       content = "Invalid file representation."
     }
     return content;
   }
+
+
+  $scope.options = {
+    loop: false,
+    effect: 'horizontal',
+    speed: 200,
+    pagination: true,
+  }
+
+  /**
+   * This controls the slider, this is the init function
+   */
+  $scope.$on("$ionicSlides.sliderInitialized", function (event, data) {
+    $scope.slider = data.slider;
+  });
+
+  /**
+   * This sets the active page and the 
+   */
+  $scope.$on("$ionicSlides.slideChangeEnd", function (event, data) {
+    $scope.activeIndex = data.slider.activeIndex;
+    $scope.previousIndex = data.slider.previousIndex;
+  });
 
 });
