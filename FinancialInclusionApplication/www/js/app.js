aa1acbb5115420f59d5c3e6b8f184b5f87fcddaa
liamo94 <lleeum@gmail.com>
2017-01-10 12:33:58 +0000

    reset changes

--- FinancialInclusionApplication/www/js/app.js
+++ FinancialInclusionApplication/www/js/app.js
@@ -81,78 +81,7 @@ fIApp.run(function ($ionicPlatform, $http, $rootScope, $cordovaSQLite) {
   });
 });
 
-fIApp.controller("ExampleController", function($scope, $cordovaSQLite, $cordovaCamera, $cordovaFile) {
-
-  $scope.images = [];
- 
-    $scope.addImage = function() {
-        var options = {
-		destinationType : Camera.DestinationType.FILE_URI,
-		sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-		allowEdit : false,
-		encodingType: Camera.EncodingType.JPEG,
-		popoverOptions: CameraPopoverOptions,
-	};
-	
-	// 3
-	$cordovaCamera.getPicture(options).then(function(imageData) {
- 
-		// 4
-		onImageSuccess(imageData);
- 
-		function onImageSuccess(fileURI) {
-			createFileEntry(fileURI);
-		}
- 
-		function createFileEntry(fileURI) {
-			window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-		}
- 
-		// 5
-		function copyFile(fileEntry) {
-			var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-			var newName = makeid() + name;
- 
-			window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
-				fileEntry.copyTo(
-					fileSystem2,
-					newName,
-					onCopySuccess,
-					fail
-				);
-			},
-			fail);
-		}
-		
-		// 6
-		function onCopySuccess(entry) {
-			$scope.$apply(function () {
-				$scope.images.push(entry.nativeURL);
-			});
-		}
- 
-		function fail(error) {
-			console.log("fail: " + error.code);
-		}
- 
-		function makeid() {
-			var text = "";
-			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
- 
-			for (var i=0; i < 5; i++) {
-				text += possible.charAt(Math.floor(Math.random() * possible.length));
-			}
-			return text;
-		}
- 
-	}, function(err) {
-		console.log(err);
-	});
-    }
- 
-    $scope.urlForImage = function(imageName) {
-        console.log("get correct path for image");
-    }
+fIApp.controller("ExampleController", function($scope, $cordovaSQLite) {
  
     $scope.insert = function(firstname, lastname) {
         var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
