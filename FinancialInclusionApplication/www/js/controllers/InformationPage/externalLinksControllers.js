e22d3565acdce6db0a6d132ea5d8c507c86f972c
Stuart <StuCuth@Outlook.com>
2017-01-27 16:54:47 +0000

    Minor changes to links/numbers and also splash screen stuff

--- FinancialInclusionApplication/www/js/controllers/InformationPage/externalLinksControllers.js
+++ FinancialInclusionApplication/www/js/controllers/InformationPage/externalLinksControllers.js
@@ -3,9 +3,9 @@ fIApp.controller('ExternalLinksCtrl', function($scope){
     $scope.content = 'External Links stored here';
 
     $scope.externalLinksList = [
-        {name: 'Facebook', website: 'http://www.facebook.com'},
-        {name: 'YouTube', website: 'http://www.youtube.com'},
-        {name: 'Twitter', website: 'http://www.twitter.com'}];
+        {name: 'Facebook', blurb:'', website: 'http://www.facebook.com'},
+        {name: 'YouTube', blurb:'', website: 'http://www.youtube.com'},
+        {name: 'Twitter', blurb:'', website: 'http://www.twitter.com'}];
 
     $scope.getExternalLinks = function(){
         return $scope.externalLinksList;
