7da14716bd613b63c1d2f42598e25320ce5faa43
liamo94 <lleeum@gmail.com>
2017-03-02 17:20:06 +0000

    Links now all fixed, numbers reading from json

--- FinancialInclusionApplication/www/js/controllers/InformationPage/externalLinksControllers.js
+++ FinancialInclusionApplication/www/js/controllers/InformationPage/externalLinksControllers.js
@@ -1,14 +1,23 @@
-fIApp.controller('ExternalLinksCtrl', function ($scope,$http) {
+fIApp.controller('ExternalLinksCtrl', function($scope, $http) {
     $scope.name = 'External Links';
     $scope.content = 'External Links stored here';
 
     $http.get('content/externalLinks.json')
-        .then(function (response) {
+        .then(function(response) {
             $scope.externalLinksList = response.data;
             console.log($scope.externalLinksList);
         });
 
-    $scope.getExternalLinks = function () {
+    $scope.getExternalLinks = function() {
         return $scope.externalLinksList;
     };
+
+    $scope.navigateToWebsite = function(urlObject) {
+        try {
+            var website = urlObject.website;
+            window.open(website, '_system', 'location=yes');
+        } catch (err) {
+            alert(err);
+        }
+    }
 });
--- FinancialInclusionApplication/www/js/controllers/informationControllers.js
+++ FinancialInclusionApplication/www/js/controllers/informationControllers.js
@@ -2,7 +2,7 @@ fIApp.controller('InformationCtrl', function($scope){
     
        $scope.name = 'Information Page';
        $scope.infoCategories = [
-            {name:'External Links', url:'externalLinks', icon:'link'},
+            {name:'External Links', url:'externalLinks', icon:'earth'},
             {name:'Useful Numbers', url:'usefulNumbers', icon:'iphone'},
             {name:'Help', url:'help', icon:'help-buoy'}];
 
