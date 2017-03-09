baf7f5c8eda0cc73f33013df50278f37b89a510d
cameroncclark <xeb12181@uni.strath.ac.uk>
2017-03-09 17:36:57 +0000

    Added In Dialog Warnings

--- CMS/CMS/src/Controller/DeleteLinkCtrl.java
+++ CMS/CMS/src/Controller/DeleteLinkCtrl.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import javax.swing.JOptionPane;
+
 import Model.Model;
 import View.LinksPanel;
 import View.MainContainer;
@@ -7,6 +9,12 @@ import View.MainContainer;
 public class DeleteLinkCtrl {
 	public DeleteLinkCtrl(Model model, MainContainer view) {
 		LinksPanel panel = (LinksPanel) view.getActivePanel();
-		model.deleteLink(panel.getSelectedDelete());
+		String deleteLink = panel.getSelectedDelete();
+		int sure = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete " + deleteLink + "?", "Are You Sure?", JOptionPane.YES_NO_OPTION,
+				JOptionPane.INFORMATION_MESSAGE);
+		if (sure == JOptionPane.YES_OPTION) {
+			model.deleteLink(deleteLink);
+			JOptionPane.showMessageDialog(null,"'" + deleteLink + "' successfully deleted!", "Deleted", JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
 }
--- CMS/CMS/src/Controller/DeleteNumberCtrl.java
+++ CMS/CMS/src/Controller/DeleteNumberCtrl.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import javax.swing.JOptionPane;
+
 import Model.Model;
 import View.MainContainer;
 import View.NumbersPanel;
@@ -7,6 +9,12 @@ import View.NumbersPanel;
 public class DeleteNumberCtrl {
 	public DeleteNumberCtrl(Model model, MainContainer view) {
 		NumbersPanel panel = (NumbersPanel) view.getActivePanel();
-		model.deleteNumber(panel.getSelectedDelete());
+		String deleteNumber = panel.getSelectedDelete();
+		int sure = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete " + deleteNumber + "?", "Are You Sure?", JOptionPane.YES_NO_OPTION,
+				JOptionPane.INFORMATION_MESSAGE);
+		if (sure == JOptionPane.YES_OPTION) {
+			model.deleteNumber(deleteNumber);
+			JOptionPane.showMessageDialog(null, "'" + deleteNumber + "' successfully deleted!", "Deleted", JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
 }
